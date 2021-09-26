5aedb571e69647ca429f84c3b60e66b5886472e0
Leah Davis <leahdavis@Leahs-MacBook-Pro.local>
2021-09-26 18:44:49 -0400

    Added JS for local storage download and recall for high scores page as well as methods to create and save scores.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,8 @@
 var time = questions.length * 15;
 var timer;
 var questionIndex = 0;
-var isWin = false;
+var currentQuestion = questions[questionIndex];
+var highScores = [];
 
 // DOM elements
 var startButton = document.querySelector("#start");
@@ -12,6 +13,7 @@ var finalScore = document.querySelector("#final-score");
 var submitButton = document.querySelector("#submit");
 var userInitials = document.querySelector("#initials")
 var storedScores = document.querySelector(".stored-scores");
+var rightWrong = document.querySelector("#right-wrong");
 
 
 // Starts the quiz
@@ -26,9 +28,8 @@ function startQuiz() {
     startTimer();
 };
 
-// Pulls first question from question index array
+// Pulls first question from question index array + gives answer choices
 function getCurrentQuestion() {
-    var currentQuestion = questions[questionIndex];
     console.log(currentQuestion);
     var titleEl = document.querySelector("#question-title");
     titleEl.textContent = currentQuestion.title;
@@ -44,70 +45,96 @@ function getCurrentQuestion() {
     } 
 }
 
-//function winGame() {
-//    var finalScore = document.querySelector("#final-score");
-//    finalScore.textContent = timerCount;
-//}
-
-//function loseGame() {
-//    var finalScore = document.querySelector("final-score");
-//    finalScore.textContent = timerCount;
-//}
-
-
-
-// Starts and stops the timer and triggers winGame() and loseGame()
+// Starts and stops the timer and run gameOver
 function startTimer() {
-    timeEl.setAttribute("value", time);
     timer = setInterval (function() {
         time--;
         timeEl.textContent = time;
-        if (time >= 0) {
-            if (isWin && time > 0) {
+        // If time is more than 0 and questions are complete, run gameOver
+        if (time > 0 && currentQuestion > questions.length) {
+            if ( time > 0) {
                 clearInterval(timer);
-                winGame();
+                gameOver();
             }
         }
-        if (timerCount === 0) {
+        if (time === 0) {
             clearInterval(timer);
-            loseGame();
+            gameOver();
         }
     }, 1000);
 };
 
-function answerQuestion() {
-    if (questions.answer = true) {
-        //move to the next question
-    } else if (questions.answer = false) {
-        //time penalty & move to next question
+// Checks whether the answer selected is correct or incorrect + deducts points appropriately
+function checkAnswer(event) {
+    event.preventDefault();
+    rightWrong.setAttribute("display: block");
+    var p = document.createElement("p");
+    rightWrong.appendChild(p);
+    
+    setTimeout(function() {
+        p.style.display = "none";
+    }, 1000);
+
+    // If correct answer is chosen, "Correct!" is displayed
+    var chosenAnswer = event.target
+    if (questions[questionIndex].answer === chosenAnswer.innerText) {
+        p.textContent = "You got it!";
+    // If incorrect answer is chosen, "Wrong!" is displayed and 10 seconds are taken from time
+    } else if (questions[questionIndex].answer !==chosenAnswer.innerText) {
+        time = time - 10;
+        p.textContent = "Good try!";
     }
+
+    // Cycles to the next question
+    if (questions.length > questionIndex + 1 || time === 0) {
+        questionIndex++;
+    }
+    getCurrentQuestion(questionIndex);
 }
 
-submitButton.addEventListener("click", function() {
-    window.open("index.html", _self);
-    for (var i = 0; i < currentQuestion.choice.length; i++) {
-        var userScore = document.createElement("div");
+// Retrieves information from user inputs and appends them to the page
+function addScore() {
+    for (i = 0; i < highScores.length; i++) {
+        var userScore = document.createElement("li");
         userScore.setAttribute("class", "userScore");
-
+        finalScore.textContent = time;
         userScore.textContent = i + 1 + ". " + userInitials + " - " + finalScore;
-        storedScores.appendChild(div);
+        storedScores.appendChild(li);
+    }
 };
 
-
-
-
-if (questions.answer = true) {
-    //move to the next question
-}   else if (questions.answer = false) {
-    // time penalty & move to next question
+// Saves user inputted score as an object and stringify's in local storage
+function saveScore() {
+    var newHighScore = {
+        initials: userInitials.value,
+        highScore: finalScore,
+    };
+    console.log(newHighScore);
+    highScores.push(newHighScore);
+    console.log(highScores);
+    localStorage.setItem("scores", JSON.stringify(highScores));
 }
-getCurrentQuestion();
 
+// Stores and retrieves arrays in/from local storage
+if (JSON.parse(localStorage.getItem("scores")) !== null) {
+    highScores = JSON.parse(localStorage.getItem("scores"));
+}
 
+// Hides questions screen and shows end screen. Also adds and stores score and initials in local storage
+function gameOver() {
+    // Hides questions screen
+    var endScreen = document.querySelector(".end-screen");
+    var questionScreen = document.querySelector(".questions");
+    questionScreen.setAttribute("class","hidden");
 
+    // Show end-screen
+    endScreen.removeAttribute("class");
 
+    // Adds + stores high score to div on highscores page + local storage
+    addScore();
+    saveScore();
+}
 
-
+// Listens for button clicks to "Start" game and "Submit" user score info
 startButton.addEventListener("click", startQuiz);
-
-
+submitButton.addEventListener("click", saveScore);
